2e9365afd24a4f262ed021e96b2c12d87c3b5d90
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-10-08 09:21:57 +0530

    mui experiment

--- src/node-entry.js
+++ src/node-entry.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server.js";
 import Root from "./root.component.js";
+import theme from "./theme";
+import { ServerStyleSheets, ThemeProvider } from "@material-ui/core/styles";
 
-export const serverRender = props => {
-  return ReactDOMServer.renderToNodeStream(<Root {...props} />);
-};
-
-export const getResponseHeaders = props => {
+export const getResponseHeaders = (props) => {
   return {
-    "x-navbar": 1
+    "x-navbar": 1,
   };
 };
+
+export function serverRender(props) {
+  const sheets = new ServerStyleSheets();
+  const htmlStream = ReactDOMServer.renderToString(
+    sheets.collect(
+      <ThemeProvider theme={theme}>
+        <Root {...props} />
+      </ThemeProvider>
+    )
+  );
+
+  // Grab the CSS from the sheets.
+  const css = sheets.toString();
+   
+  return { htmlStream, sheets: css };
+}
--- src/root.component.js
+++ src/root.component.js
@@ -1,9 +1,23 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import NavBar from "./components/navbar";
+import theme from "./theme";
 
 export default function Root(props) {
+  /*
+ // TODO - This needs to be handled a bit differently I guess
+ // adding this causes the client side to break
+React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []); 
+*/
   return (
-    <section>
-      {props.name} (overridden) is mounted! User {props.user.name}
-    </section>
+    <ThemeProvider theme={theme}>
+      <NavBar title={props.name} />
+    </ThemeProvider>
   );
 }
